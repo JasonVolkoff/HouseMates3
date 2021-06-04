10a27a46116430cb09d1ee7cfacf3b6d6ae05992
JasonVolkoff <kashijason@gmail.com>
2021-06-04 11:10:23 -0700

    Updated models for working template notifications

--- housemates_proj/login_and_profile/urls.py
+++ housemates_proj/login_and_profile/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     #### Protected Profile Routes ####
     path('profile', views.profile),
     path('profile/create_house', views.create_house),
-    path('profile/select_main_house', views.select_main_house),
+    path('profile/select_main_house/<int:house_id>', views.select_main_house),
     #### Protected House Routes ####
-    path('profile/main_house/<int:id>', views.main_house),
-    path('profile/main_house/<int:id>/add_housemate', views.add_housemate),
+    path('profile/main_house/', views.main_house),
+    path('profile/main_house/add_housemate', views.add_housemate),
 
 
 ]
